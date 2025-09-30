af271195ab116f77e9c6c27aaf1e51e392c422ca
throap <silverlightstar99@gmail.com>
2025-09-30 13:37:10 -0700

    Ultrasonic Sensor - Lab #4.ino

---
+++ Ultrasonic Sensor - Lab #4.ino
@@ -0,0 +1,76 @@
+/*
+  Ultrasonic Sensor HC-SR04 and Arduino Tutorial
+  by Dejan Nedelkovski,
+  www.HowToMechatronics.com
+*/
+
+// defines pins numbers
+const int trigPin = 11;
+const int echoPin = 12;
+
+// defines variables
+long duration;
+int distance;
+int N;
+int ultrasonic(){
+   digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+
+  // Calculating the distance
+  distance = duration * 0.034 / 2;
+  return distance;
+}
+void setup() {
+  // UltraSonoc Pins
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(9600); // Starts the serial communication
+}
+
+void loop() {
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+
+  // Calculating the distance
+  distance = duration * 0.034 / 2;
+  
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  
+  //Event IF Statment
+  if (distance>15) {
+    Serial.println("Waiting To Detect An Object");
+  }
+  else {
+    while(distance<=15) {
+      Serial.println("Object Detected & Present");
+      distance = ultrasonic();
+    }
+    Serial.println("Detected Object Is Gone");
+    N = N+1;
+    Serial.print(N);
+    Serial.println(" Objects Detected");
+  }
+}
